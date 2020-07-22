5bb841c6026f1141efd2a8dd5aba7b9237d7d2b2
Adam Desivi <desivi@desivi.com>
2020-07-22 22:09:46 +0200

    feat: add metadata keys

---
+++ src/constants/metadata-keys.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_PROPS_KEY: unique symbol = Symbol(
+  'eveble:containers:default-props'
+);
+
+export const SERIALIZABLE_LIST_PROPS_KEY: unique symbol = Symbol(
+  'eveble:container:serializable-list-props'
+);
