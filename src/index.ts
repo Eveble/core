4fc308ca4d5ef144d1405840ad8abb210c32a7bc
Adam Desivi <desivi@desivi.com>
2020-07-23 01:00:45 +0200

    style: remove comment

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ export { types as CoreTypes } from 'util';
 // Constants
 export { BINDINGS } from './constants/bindings';
 export { BINDINGS as CORE_BINDINGS } from './constants/bindings';
-// Metadata-keys
 export { METADATA_KEYS } from './constants/metadata-keys';
 export { METADATA_KEYS as CORE_METADATA_KEYS } from './constants/metadata-keys';
-
 // Components
 export { ExtendableError } from './components/extendable-error';
 export { Library } from './components/library';
